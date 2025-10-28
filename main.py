749869c121366e7270180fd01a18ba6e2aa409e0
Ian Mullins <ian.mullins.business@gmail.com>
2025-10-28 09:19:09 -0400

    fixed widget placement; window height/width now scaled based on the monitor's resolution

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from src.interface.gui import App
 
 if __name__ == "__main__":
-    app = App((600,400))
+    app = App()
--- src/interface/gui.py
+++ src/interface/gui.py
@@ -1,37 +1,57 @@
 import tkinter as tk
 from tkinter import ttk
 
+size = []
 
 class App(tk.Tk):
-    def __init__(self, size):
+    def __init__(self):
         super().__init__()
         self.title('MPTranscribe')
-        self.dimensions(size)
-        self.fileDialogue = Menu(self)    
+        self.dimensions()
+        self.fileFrame = FileDialogueFrame(self)
+        self.transFrame = TranscriberFrame(self)
 
         self.mainloop()
 
-    def dimensions(self, size):
+    def dimensions(self):
+        screen = tk.Tk()
+        size.append(int(screen.winfo_screenwidth()/2.5))
+        size.append(int(screen.winfo_screenheight()/2.5))
+        screen.destroy()
+
         self.geometry(f'{size[0]}x{size[1]}')
         self.minsize(size[0], size[1])
         self.maxsize(size[0], size[1])
 
-class Menu(tk.Frame):
+
+class FileDialogueFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.place(x=0,y=0,relwidth=0.3,relheight=1)
+        self.place(relx=0.1, rely=0.1, relwidth=0.3, relheight=1)
+        self.createWidget()
 
-        self.createWidgets()
-
-    def createWidgets(self):
+    def createWidget(self):
         self.dialogueButton = ttk.Button(self, text='Open File')
-        self.transcribeButton = ttk.Button(self, text='Start')
-
         self.setLayout()
 
     def setLayout(self):
-        self.columnconfigure((0,1,2,3,4), weight=1, uniform='a')
-        self.rowconfigure((0,1,2,3,4,5,6,7,8), weight=1, uniform='a')
+        self.columnconfigure((0,1,2,3,4,5,6), weight=1, uniform='a')
+        self.rowconfigure((0,1,2,3,4,5,6,7,8,9), weight=1, uniform='a')
 
-        self.dialogueButton.grid(row=9, column=4, padx=10, pady=10, columnspan=3, rowspan=1, sticky='nswe')
-        self.transcribeButton.grid(row=9, column=0, padx=10,pady=10, columnspan=3, rowspan=1, sticky='nswe')
+        self.dialogueButton.grid(row=8, column=0, padx=10, pady=10, columnspan=3, rowspan=1, sticky='nswe')
+
+class TranscriberFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.place(relx=0.7, rely=0.1, relwidth=0.3, relheight=1)
+        self.createWidget()
+
+    def createWidget(self):
+        self.transcribeButton = ttk.Button(self, text='Transcribe')
+        self.setLayout()
+
+    def setLayout(self):
+        self.columnconfigure((0,1,2,3,4,5,6), weight=1, uniform='a')
+        self.rowconfigure((0,1,2,3,4,5,6,7,8,9), weight=1, uniform='a')
+
+        self.transcribeButton.grid(row=8, column=0, padx=10, pady=10, columnspan=3, rowspan=1, sticky='nswe')
