bd5be3d1c9120baf0ae5ade733d790ef978feff3
Ian Mullins <ian.mullins.business@gmail.com>
2025-10-28 18:44:39 -0400

    add menubar to main App class

--- src/interface/gui.py
+++ src/interface/gui.py
@@ -1,22 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
+from src.interface.components.menu import MenuBar
 
 size = []
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+        
         self.title('MPTranscribe')
-        self.dimensions()
-        self.fileFrame = FileDialogueFrame(self)
-        self.transFrame = TranscriberFrame(self)
         self.selectedFile = None
+        self.optWinOpen = False
+        # TODO - add dark theme
+        self.theme = None
+
+        self.appSize()
+        self.config(menu=MenuBar(self))
 
         self.mainloop()
 
-    def dimensions(self):
+    def appSize(self):
         screen = tk.Tk()
         size.append(int(screen.winfo_screenwidth()/2.5))
         size.append(int(screen.winfo_screenheight()/2.5))
@@ -25,44 +28,3 @@ class App(tk.Tk):
         self.geometry(f'{size[0]}x{size[1]}')
         self.minsize(size[0], size[1])
         self.maxsize(size[0], size[1])
-
-
-class FileDialogueFrame(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.place(relx=0.1, rely=0.1, relwidth=0.3, relheight=1)
-        self.createWidget()
-
-    def createWidget(self):
-        self.dialogueButton = ttk.Button(self, text='Open File', command=self.getFileName)
-        self.setLayout()
-
-    def setLayout(self):
-        self.columnconfigure((0,1,2,3,4,5,6), weight=1, uniform='a')
-        self.rowconfigure((0,1,2,3,4,5,6,7,8), weight=1, uniform='a')
-
-        self.dialogueButton.grid(row=7, column=0, padx=10, pady=10, columnspan=2, rowspan=1, sticky='nswe')
-    
-    def getFileName(self):
-        fileTypes = [
-            ('All files', '*.*')
-        ]
-        self.selectedFile = fd.askopenfilename(title='Choose an audio file', initialdir='/', filetypes=fileTypes)
-        self.master.selectedFile = self.selectedFile
-
-class TranscriberFrame(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.place(relx=0.7, rely=0.1, relwidth=0.3, relheight=1)
-        self.createWidget()
-
-    def createWidget(self):
-        self.transcribeButton = ttk.Button(self, text='Transcribe', command=self.printFile)
-        self.setLayout()
-
-    def setLayout(self):
-        self.columnconfigure((0,1,2,3,4,5,6), weight=1, uniform='a')
-        self.rowconfigure((0,1,2,3,4,5,6,7,8), weight=1, uniform='a')
-
-        self.transcribeButton.grid(row=7, column=0, padx=10, pady=10, columnspan=2, rowspan=1, sticky='nswe')
-    
