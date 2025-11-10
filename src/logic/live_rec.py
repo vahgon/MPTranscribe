c109962d486838d63c0a21246c41a30d94261d88
Ian Mullins <ian.mullins.business@gmail.com>
2025-11-09 21:30:03 -0500

    test changing dotenv vals

--- src/logic/live_rec.py
+++ src/logic/live_rec.py
@@ -1,3 +1,7 @@
 import speech_recognition as sr
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+from user_settings import UserSettings
+# Testing .config, init for it will be moved later
+
+settings = UserSettings()
+
+settings.changeConfigOption(opt='MICROPHONE', val='2')
