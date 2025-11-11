7d33d3c41df654f0dbc39fc3c80c051db9c32b33
Ian Mullins <ian.mullins.business@gmail.com>
2025-11-11 18:12:56 -0500

    added test for fasterwhisper model

--- tests/logic_test.py
+++ tests/logic_test.py
@@ -1,5 +1,14 @@
-from src.logic.audio.live_rec import MicRecording
+from src.logic.transcribe import Transcriber
 from speech_recognition import AudioData
 
-def test_mic_class():
-    assert type(MicRecording().RecAudio()) == AudioData, "Function did not return object of type AudioData" 
+def test_mic_return_audio():
+    micAudio = Transcriber()
+    micAudio.micLiveAudio()
+    assert type((micAudio.audio)) == AudioData, "Function did not return object of type AudioData" 
+
+def test_fasterwhisper():
+    fwhsper = Transcriber()
+    fwhsper.micLiveAudio()
+    result = fwhsper.fasterWhisper()
+
+    assert type((result)) == str, "Faster Whisper did not return string"
