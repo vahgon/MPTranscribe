d74e42f731a670abab47c3e99ca169730f883807
Ian Mullins <ian.mullins.business@gmail.com>
2025-11-11 18:12:31 -0500

    renamed to mic_rec

--- src/logic/audio/mic_rec.py
+++ src/logic/audio/mic_rec.py
@@ -1,14 +1,11 @@
-from speech_recognition import Recognizer, AudioData
-from src.logic.audio.microphone import UserMicrophone
-from src.settings.user_settings import UserSettings
-
-UserSettings()
+from speech_recognition import AudioData, Recognizer
+from src.settings.mic_set import UserMicrophone
 
 class MicRecording(Recognizer):
     def __init__(self):
         super().__init__()
 
-    def RecAudio(self) -> AudioData:
+    def recAudio(self) -> AudioData:
         with UserMicrophone() as source:
             return self.listen(source)
 
